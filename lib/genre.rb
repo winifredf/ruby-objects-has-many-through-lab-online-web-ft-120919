f4c88a6bb66845533ab457a772d5b94755819150
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-13 11:32:38 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -15,5 +15,15 @@ class Genre
     @@all
   end
   
-  
+  def new_song(name, artist)
+    Song.new(name, artist, self)
+  end
+
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
+
+  def artists
+    songs.collect {|song| song.artist}
+  end
 end
