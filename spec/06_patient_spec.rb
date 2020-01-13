e7167cec82607c7ed67b55106ba30264b9a36012
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-13 12:55:08 +0000

    Automatically backed up by Learn

--- spec/06_patient_spec.rb
+++ spec/06_patient_spec.rb
@@ -11,7 +11,7 @@ describe 'Patient' do
     it 'given a doctor and date, creates a new appointment belonging to that patient' do
       doctor_who = Doctor.new('The Doctor')
       hevydevy = Patient.new('Devin Townsend')
-      #appointment = hevydevy.new_appointment(doctor_who, 'Friday, January 32nd')
+      appointment = hevydevy.new_appointment(doctor_who, 'Friday, January 32nd')
 
       expect(hevydevy.appointments).to include(appointment)
       expect(appointment.patient).to eq(hevydevy)
