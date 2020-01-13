46fb99c75b3e17cfffdbc1b14771278aece30ae9
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-13 11:54:37 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,7 +4,7 @@ class Appointment
   
   @@all = []
   
-  def initialize(date, patient, doctor)
+  def initialize(date = "", patient = "", doctor = "")
     @date = date
     @patient = patient
     @doctor = doctor
