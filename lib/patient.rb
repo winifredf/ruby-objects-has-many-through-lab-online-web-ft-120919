5276335ddd696a8821d7a6c9f1b8fd1904dd5a3a
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-13 12:00:34 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,27 +1,24 @@
 class Patient
-  
-  attr_accessor :name, :appointment, :doctor
-  
-  @@all = []
-  
+
+  attr_reader :name, :appointment, :doctor
+
   def initialize(name)
     @name = name
-    @@all << self
-  end
-  
-  def self.all
-    @@all
-  end
-  
-  def appointments
-    Appointment.all.select {|appointment| appointment.patient == self}
-  end
-
-  def doctors
-    appointments.collect {|appointment| appointment.doctor}
   end
 
   def new_appointment(doctor, date)
-    Appointment.new(self, date, doctor)
+    Appointment.new(self, doctor, date)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
+  def doctors
+    Appointment.all.map do |appointment|
+      appointment.doctor
+    end
   end
 end
