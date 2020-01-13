93cb7204eb5761e7faff0b9247bad9aa7866a16a
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-13 12:14:10 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,19 +13,7 @@ class Patient
     @@all
   end
 
-  def new_appointment(doctor, date)
-    Appointment.new(self, doctor, date)
-  end
-
-  def appointments
-    Appointment.all.select do |appointment|
-      appointment.patient == self
-    end
-  end
-
-  def doctors
-    Appointment.all.map do |appointment|
-      appointment.doctor
+  
     end
   end
 end
