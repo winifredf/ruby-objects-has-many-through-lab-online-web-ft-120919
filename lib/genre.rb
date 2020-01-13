9fd827fb50fb5f219d01fb2a814128fc3834bc59
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-13 11:59:54 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,29 +1,21 @@
 class Genre
-  
-  attr_accessor :name, :song, :artist
-  
-  @@all = []
-  
+
+  attr_reader :name, :genre
+  attr_accessor :song
+
   def initialize(name)
     @name = name
-    @songs = []
-    @artist = artist
-    @@all << self
-  end
-  
-  def self.all
-    @@all
-  end
-  
-  def new_song(name, artist)
-    Song.new(name, artist, self)
   end
 
   def songs
-    Song.all.select {|song| song.genre == self}
+    Song.all.select do |song|
+      song.genre == self
+    end
   end
 
   def artists
-    songs.collect {|song| song.artist}
+    Song.all.map do |song|
+      song.artist
+    end
   end
 end
