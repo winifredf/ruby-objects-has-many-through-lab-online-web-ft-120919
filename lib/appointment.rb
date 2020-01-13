ccfddf9c582fcc3b8c58da5ae124682dd51d4fd1
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-13 11:02:52 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,12 @@
+class Appointment
+  
+  attr_accessor :date, :patient, :doctor
+  
+  @@all = []
+  
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+  end
+end
